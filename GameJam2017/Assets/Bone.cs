6f5527a06172825413ef36b2e9f18ef3c1bac82c
Viktoriya Voinova <vicky.voin@gmail.com>
2017-01-21 14:28:17 -0500

    Finished the hat physics

---
+++ GameJam2017/Assets/Bone.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bone : MonoBehaviour
+{
+    float targetWorldYPos;
+    public float TargetWorldYPos
+    {
+        get { return targetWorldYPos; }
+        set { targetWorldYPos = value; previousWorldYPos = transform.position.y; timePassed = 0.0f; }
+    }
+
+    float previousWorldYPos;
+    float timePassed;
+
+    const float speed = 0.2f;
+
+
+    void Start()
+    {
+        previousWorldYPos = 0.0f;
+        timePassed = speed;
+        previousWorldYPos = targetWorldYPos;
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if (transform.GetSiblingIndex() > 0)
+            transform.LookAt(transform.parent.GetChild(transform.GetSiblingIndex() - 1));
+
+        timePassed += Time.deltaTime;
+
+        if (timePassed >= speed)
+        {
+            previousWorldYPos = targetWorldYPos;
+            timePassed = speed;
+            transform.position = new Vector3(transform.position.x, targetWorldYPos, transform.position.z);
+
+        }
+        else
+        {
+            float yPos = Mathf.Lerp(previousWorldYPos, targetWorldYPos, timePassed / speed);
+            transform.position = new Vector3(transform.position.x, yPos, transform.position.z);
+        }
+        
+        if ( timePassed > speed/2)
+        {
+            if (transform.parent.childCount > transform.GetSiblingIndex() + 1)
+                transform.parent.GetChild(transform.GetSiblingIndex() + 1).GetComponent<Bone>().TargetWorldYPos = targetWorldYPos;
+        }
+    }
+}
---
+++ GameJam2017/Assets/HeadController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeadController : MonoBehaviour
+{
+    public float sensitivity = 1.0f;
+    public Bone rootBone;
+
+    bool isAscending;
+
+    void Start()
+    {
+        StartCoroutine(SetBonePosition());
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        transform.position += new Vector3(0, Input.GetAxis("Vertical"), 0)*sensitivity;
+
+        isAscending = Input.GetAxis("Vertical") > 0;
+
+	}
+    
+    IEnumerator SetBonePosition()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(0.25f);
+            rootBone.TargetWorldYPos = transform.position.y;
+
+        }
+    }
+}
